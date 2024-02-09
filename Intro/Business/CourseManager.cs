f972420142ec8d2574692a3534750a32d79e8ab6
Murat Sivil <murat.sivil@im-par.de>
2024-02-09 15:53:50 +0300

    first commit

---
+++ Intro/Business/CourseManager.cs
@@ -0,0 +1,42 @@
+﻿using Intro.Entities;
+
+namespace Intro.Bussiness;
+
+public class CourseManager
+{
+    readonly Course[] courses = new Course[3];
+    public CourseManager()
+    {
+        Console.WriteLine("Loading...");
+        Course course1 = new()
+        {
+            Id = 1,
+            Name = "CSharp",
+            Description = ".NET 8",
+            Price = 0
+        };
+
+        Course course2 = new()
+        {
+            Id = 1,
+            Name = "Java",
+            Description = ".NET 8",
+            Price = 0
+        };
+
+        Course course3 = new()
+        {
+            Id = 1,
+            Name = "Python",
+            Description = ".NET 8",
+            Price = 0
+        };
+
+        courses[0] = course1;
+        courses[1] = course2;
+        courses[2] = course3;
+    }
+    public Course[] GetAll(){
+        return courses;
+    }
+}
---
+++ Intro/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Intro.Bussiness;
+
+
+var courseManager = new CourseManager();
+var allCourses = courseManager.GetAll();
+foreach (var course in allCourses)
+{
+    Console.WriteLine($"{course.Name} / {course.Price}");
+}
+Console.WriteLine("All courses are available");
+Console.WriteLine("Code is over");
